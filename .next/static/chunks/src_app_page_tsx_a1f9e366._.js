d77faecab6088479981b05a04e8b84ff67304d82
Tamanna Bhatti <tamanna@ma.iitr.ac.in>
2025-06-24 01:15:05 +0530

    Add .next/static/chunks/src_app_page_tsx_a1f9e366._.js 1750707904471

---
+++ .next/static/chunks/src_app_page_tsx_a1f9e366._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_350b5788._.js",
+  "static/chunks/src_204dba1f._.js"
+],
+    source: "dynamic"
+});
